62648b823883716cba324663476b7660a2130b0a
Ahmed Silini <ahmedsilini5@gmail.com>
2025-04-20 11:26:00 +0100

    added messages list

--- lib/my_home_page.dart
+++ lib/my_home_page.dart
@@ -1,3 +1,4 @@
+import 'package:chatbot/message.dart';
 import 'package:flutter/material.dart';
 
 class MyHomePage extends StatefulWidget {
@@ -10,8 +11,48 @@ class MyHomePage extends StatefulWidget {
 }
 
 class _MyHomePageState extends State<MyHomePage> {
+  
+  final List<Message> _messages = [
+    Message(text: "Hi", isUser: true),
+    Message(text: "Hello", isUser: false),
+    Message(text: "Yoyys", isUser: true),
+    Message(text: "Boom", isUser: false),
+  ];
+
   @override
   Widget build(BuildContext context) {
-    return Scaffold(body: Center(child: Text('Welcome to Flutter!'),),);
+    return Scaffold(
+      body: Column(
+        children: [
+          Expanded(
+            child: ListView.builder(
+              itemCount: _messages.length,
+              itemBuilder: (context, index) {
+                final message = _messages[index];
+                return ListTile(
+                  title: Align(
+                    alignment: message.isUser ? Alignment.centerRight : Alignment.centerLeft,
+                    child: Container(
+                      padding: const EdgeInsets.all(8.0),
+                      decoration: BoxDecoration(
+                        color: message.isUser ? Colors.blue : Colors.grey[300],
+                        borderRadius: BorderRadius.circular(8.0),
+                      ),
+                      child: Text(message.text,
+                        style: TextStyle(
+                          color: message.isUser ? Colors.white : Colors.black,
+                          fontWeight: FontWeight.bold,
+                        ),
+                      ),
+                    ),
+                  ),
+                );
+              },
+            ),
+          ),
+       
+        ],
+      ),
+    );
   }
 }
